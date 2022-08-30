b94cfacd55da760030efe0e3a9944e4dd05b4b1f
Mehdi <mehdi.ouledhali@gmail.com>
2022-08-30 17:50:28 +0100

    before breaking changes

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
+import gsap from 'gsap'
+
+let tl = new gsap.timeline();
+
+
+tl.fromTo("#ideas", {y: 100,opacity:0}, {y: 0,opacity:1, duration: 1,ease: "power4.inOut"})
+.fromTo("#worth", {y: 100,opacity:0}, {y: 0,opacity:1, duration: 1,ease: "power4.inOut" },'-=0.9')
+.fromTo("#spreading", {y: 100,opacity:0}, {y: 0,opacity:1, duration: 1,ease: "power4.inOut"},"-=0.85");
