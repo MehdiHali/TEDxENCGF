32c83794cec9046aaaabaf326addeb9e442cc92c
Mehdi <mehdi.ouledhali@gmail.com>
2022-09-26 16:10:55 +0100

    animate last section on mobile also

--- src/index.js
+++ src/index.js
@@ -189,14 +189,6 @@ gsap.fromTo("#mlb2-1414815",{y:100,autoAlpha: 0},{scrollTrigger:{
   markers: false,
   scrub: 1
 },y:0,autoAlpha: 1,duration: 2, ease: "power1.inOut"})
-gsap.fromTo("#get-involved",{y:100, autoAlpha: 0},{scrollTrigger:{
-  trigger: "#get-involved",
-  pin: false,
-  start: "top bottom",
-  end: "top top",
-  scrub: 1,
-  markers: false
-},y:0, autoAlpha: 1, duration: 1, ease: "power2.inOut"})
 
 
 
@@ -213,3 +205,13 @@ gsap.fromTo("#get-involved",{y:100, autoAlpha: 0},{scrollTrigger:{
       tedxVideo.classList.remove("hidden")
       console.log(tedxVideo)
   })
+
+
+gsap.fromTo("#get-involved",{y:100, autoAlpha: 0},{scrollTrigger:{
+  trigger: "#get-involved",
+  pin: false,
+  start: "top bottom",
+  end: "top top",
+  scrub: 1,
+  markers: false
+},y:0, autoAlpha: 1, duration: 1, ease: "power2.inOut"})
